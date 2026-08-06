dc16fb36f2c1034132714660ae06b2446f555755
Ary Kleinerman <kleinerman@gmail.com>
2026-08-06 20:22:46 -0300

    Adjust logo size and colors

--- src/components/AppLogo.tsx
+++ src/components/AppLogo.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export function AppLogo() {
-  return <Image className="app-logo" src="/vuel-logo.svg" alt="" aria-hidden="true" width={250} height={136} priority />;
+  return <Image className="app-logo" src="/vuel-logo.svg" alt="" aria-hidden="true" width={320} height={175} priority />;
 }
