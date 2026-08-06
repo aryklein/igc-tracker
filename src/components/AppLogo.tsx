678c2481c318ecef5aed4b6a697b7e8977805162
Ary Kleinerman <kleinerman@gmail.com>
2026-08-06 15:47:30 -0300

    Make logo decorative for screen readers

--- src/components/AppLogo.tsx
+++ src/components/AppLogo.tsx
@@ -1,6 +1,6 @@
 export function AppLogo() {
   return (
-    <svg className="app-logo" viewBox="0 0 96 64" role="img" aria-label="vuel.in logo">
+    <svg className="app-logo" viewBox="0 0 96 64" aria-hidden="true">
       <path className="app-logo-wing" d="M13 29C25 10 66 7 83 25C61 20 37 21 13 29Z" />
       <path className="app-logo-line" d="M40 26L34 43" />
       <path className="app-logo-line" d="M56 25L62 43" />
